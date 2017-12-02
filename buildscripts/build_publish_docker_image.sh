d54d507317f229a1b34a168c8ffbca0299edf92e
Rob Swartenbroekx <rob.s@sparkcentral.com>
2017-12-02 21:32:34 +0100

    fix build file

--- buildscripts/build_publish_docker_image.sh
+++ buildscripts/build_publish_docker_image.sh
@@ -2,9 +2,8 @@ cd $HOME/build/robisrob/kafka_experiment
 mkdir docker_workdir
 ./gradlew :application:distTar
 cp buildscripts/Dockerfile ./docker_workdir
-tar -xf application/build/distributions/application.tar
-cp application/build/distributions/application ./docker_workdir
 cd docker_workdir
+tar -xf ../application/build/distributions/application.tar
 docker build -t robisrob/kafka-experiment:$TRAVIS_BUILD_NUMBER .
 docker login -u=$HEROKU_USERNAME -p=$HEROKU_AUTH_TOKEN registry.heroku.com;
 docker tag robisrob/kafka-experiment:$TRAVIS_BUILD_NUMBER registry.heroku.com/kafka-experiment/web;
