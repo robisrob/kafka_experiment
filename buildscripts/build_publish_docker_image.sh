c35adbfe34d43cde5936e0231da6613645847ed3
Rob Swartenbroekx <rob.s@sparkcentral.com>
2017-12-02 21:42:13 +0100

    push image also to dockerhub

--- buildscripts/build_publish_docker_image.sh
+++ buildscripts/build_publish_docker_image.sh
@@ -6,5 +6,7 @@ cd docker_workdir
 tar -xf ../application/build/distributions/application.tar
 docker build -t robisrob/kafka-experiment:$TRAVIS_BUILD_NUMBER .
 docker login -u=$HEROKU_USERNAME -p=$HEROKU_AUTH_TOKEN registry.heroku.com;
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker push robisrob/kafka-experiment:$TRAVIS_BUILD_NUMBER
 docker tag robisrob/kafka-experiment:$TRAVIS_BUILD_NUMBER registry.heroku.com/kafka-experiment/web;
 docker push registry.heroku.com/kafka-experiment/web;
